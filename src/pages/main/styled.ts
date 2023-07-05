2c047c59eb63c92ade9318f8ed299736b6fe0d26
tetianamishatkina98 <disiosibi98@gmail.com>
2023-07-05 18:30:58 +0200

    wip

--- src/pages/main/styled.ts
+++ src/pages/main/styled.ts
@@ -7,8 +7,8 @@ export const StyledLeftSide = styled.div`
   flex: 1;
   min-width: 300px;
   height: 100vh;
-  background: rgba(29, 78, 216, 0.15);
-  box-shadow: 15px 0px 10px 0px rgba(29, 78, 216, 0.15);
+  background: #170d25;
+  box-shadow: 15px 0px 10px 0px #170d25;
 `;
 
 export const StyledRightSide = styled.div`
@@ -35,14 +35,24 @@ export const StyledMain = styled.div`
 export const StyledMock = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url("/public/logo/javascript-1.1024x780.png");
-
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
   z-index: 0;
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: space-between;
+  position: relative;
+
+
+  &::after{
+    position: absolute;
+    content: '';
+    background-image: url("/logo/typescript.png");
+    background-size: 100px;
+    opacity:10%;
+    width: 100%;
+    height: 100vh;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+  }
 `;
