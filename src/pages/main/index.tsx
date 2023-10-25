bb820cc2f52be9a2befa24a3e1d94ae2d0fa8a4f
tetianamishatkina98 <disiosibi98@gmail.com>
2023-10-25 13:40:34 +0200

    wip

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useEffect, useState } from "react";
 import {
-  StyledLeftSide,
-  StyledRightSide,
-  StyledMain,
-  StyledMock,
+	StyledLeftSide,
+	StyledRightSide,
+	StyledMain,
+	StyledMock,
 } from "./styled";
 import { UserCard } from "../../components/userCard";
 import { ProjectCard } from "../../components/projectCard";
@@ -13,55 +13,55 @@ import { StartPage } from "../startPage";
 import { HoverLight } from "../../components/hoverLight";
 import { StarsBackground } from "../../components/starsBackground";
 
-
 export const Main: FC<{ handleSeeMore: VoidFunction; isSeeMore: boolean }> = ({
-  handleSeeMore,
-  isSeeMore,
+	handleSeeMore,
+	isSeeMore,
 }) => {
-  const data = useMyProjects();
-  const [hasMore, setHasMore] = useState(false);
+	const data = useMyProjects();
+	const [hasMore, setHasMore] = useState(false);
 
-  useEffect(() => {
-    if (isSeeMore) {
-      setTimeout(() => {
-        setHasMore(true);
-      }, 2800);
-    }
-  }, [isSeeMore]);
+	useEffect(() => {
+		if (isSeeMore) {
+			setTimeout(() => {
+				setHasMore(true);
+			}, 2800);
+		}
+	}, [isSeeMore]);
 
-  return (
-    <StyledMain>
-      {!hasMore ? (
-        <StartPage
-          handleSeeMore={handleSeeMore}
-          isSeeMore={isSeeMore}
-          avatar={data.map((user) => user.owner.avatar_url)}
-        />
-      ) : (
-        <StyledMain id="test">
-          <StyledLeftSide>
-            <UserCard avatar={data.map((user) => user.owner.avatar_url)} />
-          </StyledLeftSide>
-          <StyledRightSide>
-            <StyledMock>
-              {data?.filter((pr) => {
-                return pr.name !== "portfolio"
-              }).map((project) => {
-
-                return (
-                  <ProjectCard
-                    language={project.language}
-                    key={project.id}
-                    name={project.name}
-                    description={project.description || ""}
-                    repoLink={project.html_url}
-                  />
-                );
-              })}
-            </StyledMock>
-          </StyledRightSide>
-        </StyledMain>
-      )}
-    </StyledMain>
-  );
+	return (
+		<StyledMain>
+			{!hasMore ? (
+				<StartPage
+					handleSeeMore={handleSeeMore}
+					isSeeMore={isSeeMore}
+					avatar={data.map((user) => user.owner.avatar_url)}
+				/>
+			) : (
+				<StyledMain>
+					<StyledLeftSide>
+						<UserCard avatar={data.map((user) => user.owner.avatar_url)} />
+					</StyledLeftSide>
+					<StyledRightSide>
+						<StyledMock>
+							{data
+								?.filter((pr) => {
+									return pr.name !== "portfolio";
+								})
+								.map((project) => {
+									return (
+										<ProjectCard
+											language={project.language}
+											key={project.id}
+											name={project.name}
+											description={project.description || ""}
+											repoLink={project.html_url}
+										/>
+									);
+								})}
+						</StyledMock>
+					</StyledRightSide>
+				</StyledMain>
+			)}
+		</StyledMain>
+	);
 };
--- src/pages/main/styled.ts
+++ src/pages/main/styled.ts
@@ -1,51 +1,70 @@
 import styled from "styled-components/macro";
 
+const mobileBreakpoint = "768px";
+
 export const StyledLeftSide = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-  position: relative;
-  z-index: 3;
-  min-width: 300px;
-  height: 100vh;
-  background: #170d25;
-  box-shadow: 15px 0px 10px 0px #170d25;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex: 1;
+	position: relative;
+	z-index: 3;
+	min-width: 300px;
+	height: 100vh;
+	background: #170d25;
+	box-shadow: 15px 0px 10px 0px #170d25;
+
+	@media (max-width: ${mobileBreakpoint}) {
+		height: auto;
+		min-width: 100%;
+		box-shadow: none;
+	}
 `;
 
 export const StyledRightSide = styled.div`
-  //display: grid;
-  //grid-template-columns: repeat(2, 1fr);
-  //grid-template-rows: repeat(2, 1fr);
-  gap: 1px;
-  //padding-left: 50px;
-  justify-content: center; /* Horizontally centers the items */
-  align-items: center;
-  flex: 3;
-  min-width: 500px;
-  height: 100vh;
-  overflow: scroll;
+	gap: 1px;
+	justify-content: center;
+	align-items: center;
+	flex: 3;
+	min-width: 500px;
+	height: 100vh;
+	overflow: scroll;
+
+	@media (max-width: ${mobileBreakpoint}) {
+		height: auto;
+		min-width: 100%;
+	}
 `;
 
 export const StyledMain = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-  height: 100vh;
-  background: #170d25;
-  overflow: hidden;
+	display: flex;
+	width: 100%;
+	flex-wrap: wrap;
+	height: 100vh;
+	background: #170d25;
+	overflow: hidden;
+
+	@media (max-width: ${mobileBreakpoint}) {
+		flex-direction: column;
+		height: auto;
+		overflow: visible;
+	}
 `;
 
 export const StyledMock = styled.div`
-  width: 100%;
-  z-index: 0;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: space-between;
-  position: relative;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  overflow: scroll;
+	width: 100%;
+	z-index: 0;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: space-between;
+	position: relative;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	overflow: scroll;
+
+	@media (max-width: ${mobileBreakpoint}) {
+		flex-direction: column;
+	}
 `;
